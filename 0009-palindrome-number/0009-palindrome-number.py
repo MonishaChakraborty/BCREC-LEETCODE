ece63eb942dd03788c048a7c655ad1887b8b4b1b
Monisha Chakraborty <monishachakrabarty84@gmail.com>
2026-05-05 16:24:18 +0530

    Time: 1 ms (92.7%), Space: 19.1 MB (86.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        reversed =str(x)[::-1]
+        if(str(x) == reversed):
+            return True
+        else:
+            return False
+        
